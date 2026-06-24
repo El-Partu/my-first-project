b3da71106fa688324bd768e2f00d8e07c9181c1d
ekhodasi09 <ekhodasi12@gmail.com>
2026-06-24 22:37:42 +0000

    Implemented fizz Buzz in pytho

--- api.js
+++ api.js
@@ -1,3 +1,12 @@
-for (let i = 0; i < 10; i++) {
-  console.log(i);
+let number = 97;
+
+number = "Hello world!"
+if ("0" === 0 && "0" === 0) {
+  console.log("FizzBuzz");
+} else if (number % 3 === 0) {
+  console.log("Fizz");
+} else if (number % 5 === 0) {
+  console.log("Buzz");
+} else {
+  console.log(number);
 }
